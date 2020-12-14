1a78990a13e0f6c1d88dc75c5479205ccad8bc65
Huỳnh Bùi Thanh Tân <huynhbuithanhtan@Huynhs-MacBook-Air.local>
2020-12-14 23:38:33 +0700

    lan 5

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 
 export default class Home extends React.Component {
-
 render(){
   return (
     <View style={styles.container}>             
@@ -13,7 +13,21 @@ render(){
                   }}
                   style={styles.imgCaNhan}
                 />        
-       <View style={styles.viewMiddle}></View>    
+       <View style={styles.viewMiddle}>
+          <View style={styles.imgAvatar}>
+          <Image
+                  source={{
+                    uri: 'http://ttkddongthap.vn/tainguyen/images/anhdoan.jpg',
+                  }}
+                  style={styles.imgAvatar}
+                />
+          </View>
+          <View style={styles.viewInfo}>
+            <Text style={styles.txtHoten}>Truong Van Doan</Text>
+            <Text style={styles.txtDonvi}>PBH Chau Thanh</Text>
+          </View>
+      
+         </View>    
        <View style={styles.viewBottom}></View>     
     </View>
   );
@@ -25,23 +39,42 @@ const styles = StyleSheet.create({
   },
 
    viewMiddle:{    
-    backgroundColor: 'blue', 
+    flexDirection:'row',
+    backgroundColor: '#348ceb', 
+    alignItems:'center',
     height:100,   
-    marginTop:10
+    marginTop:3
   },
 
   viewBottom :{
     flex:1,    
-    backgroundColor: 'gray',       
-    padding:10,
-    marginTop:10
-    
+    backgroundColor: '#0698c4',       
+    marginTop:3
   },
-
   imgCaNhan: {
     flex: 1,
     resizeMode: 'cover',
     height:300
+  },
+
+  viewInfo:{
+    padding:10
+  },
+
+  txtHoten:{
+    fontSize:14,
+    color: 'white', 
+  },
+  txtDonvi:{
+    fontSize:12,
+    color: '#d0d6d9', 
+  },
+
+  imgAvatar: {
+    height: 70,
+    width: 70,
+    borderRadius: 50,
+    padding:5
   },
 
 });
