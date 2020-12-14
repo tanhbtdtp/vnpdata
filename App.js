92c3d3e5871f2b1d0a6bd2e9672e8f3a463f63fb
thanhtan may 10.93.33.9 <tanhbt.dtp@gmail.com>
2020-12-14 16:26:07 +0700

    lan 3

--- App.js
+++ App.js
@@ -2,24 +2,43 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class Test extends React.Component {
 render(){
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style={styles.v1}></View>
+       <View style={styles.v2}></View>
+       <View style={styles.v3}></View>
+
+
     </View>
   );
-
 }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',   
   },
+  v1:{
+    flex:1,
+    backgroundColor: 'blue',  
+    height:100,  
+  },
+  v2:{  
+    backgroundColor: 'yellow',
+    position:'absolute',
+    height:100,
+    width:100,  
+    marginTop:40,
+    marginLeft:30
+  },
+  v3:{
+    flex:1,
+    backgroundColor: 'red',
+  }
+
 });
